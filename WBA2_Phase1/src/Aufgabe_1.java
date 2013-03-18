7cfa45b11c8e9f45103fb2e7084b52bd9d206106
Mintbox <mintboxes@googlemail.com>
2013-03-18 18:40:29 +0100

    Projekt angelegt & Aufgabe 1 eingefügt.

---
+++ WBA2_Phase1/src/Aufgabe_1.java
@@ -0,0 +1,30 @@
+
+public class Aufgabe_1 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+	
+		/**
+		Wohlgeformtheit
+		Ein XML Dokument heißt "wohlgeformt" wenn es alle XML-Regeln einhält:
+		- Es gibt genau ein Wurzelelement
+		- Elemente haben einen öffnenden und schließenden Tag
+		- Attributwerte sind durch ' oder " eingeschlossen
+		- Alle Elemente die nicht das Wurzelelement darstellen müssen vpm umgebenden Element eingeschlossen sein
+		- Alle Zeichen entsprechen dem passenden Zeichensatz
+	
+		Namespaces
+		XML-Namespaces werden genutzt, um das Vokabular eines XML-Dokumentes eindeutig zu identifizieren. Des weiteren nutzt man Namespaces um die Möglichkeit zu nutzen mehrere XML-Sprachen in einem einzelnen Dokument zu mischen.
+	
+		Beispiel von Wikipedia dazu:
+		Zum Beispiel beschreibt das <p>-Element in XHTML einen Absatz; in einer XML-Sprache für eine Personendatenbank könnte <p> ein Element für eine Person darstellen. Namensräume erlauben, diese Elemente eindeutig zu unterscheiden.
+	
+		Validität
+		Soll XML für den Datenaustausch verwendet werden, ist es von Vorteil, wenn das Format mittels Grammatik (XML-Schema) definiert ist. Ein XML-Dokument ist valid (gültig), wenn es wohlgeformt ist, den Verweis auf eine Grammatik enthält und das durch die Grammatik beschriebene Format einhält.
+		 */
+
+	}
+
+}
