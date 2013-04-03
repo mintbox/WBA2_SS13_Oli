a152c03f6cc689f330bf68d367a032b4b95bb5c6
Mintbox <mintboxes@googlemail.com>
2013-04-03 17:43:56 +0200

    Krank coden ist nicht toll. Steh aufm Schlauch.

---
+++ WBA2_Phase1/src/marshalling/Aufgabe_4b.java
@@ -0,0 +1,35 @@
+package marshalling;
+
+import java.io.FileInputStream;
+import javax.xml.bind.*;
+import generated.*;
+
+
+public class Aufgabe_4b{
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) throws Exception {
+		// TODO Auto-generated method stub
+        JAXBContext jc = JAXBContext.newInstance(Rezepte.class);
+        Unmarshaller um = jc.createUnmarshaller();
+
+        generated.Rezepte r = (Rezepte) um.unmarshal(new FileInputStream("src/Aufgabe_3a.xml"));
+
+        System.out.println("Rezepte:");
+        for (Rezepte.Rezept rez:r.getRezept()){
+            System.out.println("ID: " + rez.getId());
+    /*        System.out.println("Titel: " + rez.);
+            System.out.println("Autor: " + );
+            System.out.println("Bild: " + );
+            System.out.println("Datum: " + );
+            System.out.println("Zutaten: " + );
+            System.out.println("Zubereitung: " + );
+            System.out.println("Kommentare: " + rez.Kommentare.Kommentar);
+            System.out.println();        */
+        }
+
+	}
+
+}
